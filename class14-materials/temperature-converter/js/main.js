c2c9cf2e623a40969ddfbc0cbc896663ab7f4ccb
Radar3759 <chainmailgirl@gmail.com>
2023-06-19 16:55:36 -0700

    -add Class 14 base files

---
+++ class14-materials/temperature-converter/js/main.js
@@ -0,0 +1,10 @@
+//Write your pseduo code first! 
+//  get a starting temp
+
+   // convert it to a different temp scale
+    temp = temp * 9/5 + 32
+   // return/output the converted temp
+    
+
+    
+
