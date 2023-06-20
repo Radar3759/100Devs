3bf012fac696d4f2bbcafc643ee76ad8a053a1ba
Radar3759 <chainmailgirl@gmail.com>
2023-06-20 13:38:41 -0700

    -updt bach, simple fx, temp convert

--- class14-materials/simple-functions/js/main.js
+++ class14-materials/simple-functions/js/main.js
@@ -1,5 +1,19 @@
 //create a function that adds two numbers and alerts the sum
+function add(num1, num2) {
+    alert(num1 + num2)
+}
+
+add(2,3)
 
 //create a function that multiplys three numbers and console logs the product
+function multiplyThreeNumbers(num1, num2, num3) {
+    console.log(num1 * num2 * num3)
+}
+multiplyThreeNumbers(2, 50, 3)
+//create a function that divides two numbers and returns the quotient???
+function divideTwoNumbers(num1, num2) {
+    quot = num1 / num2
+    return (quot)
+}
 
-//create a function that divides two numbers and returns the ???
+console.log(divideTwoNumbers(50,2))
--- class14-materials/temperature-converter/js/main.js
+++ class14-materials/temperature-converter/js/main.js
@@ -1,10 +1,17 @@
 //Write your pseduo code first! 
+document.querySelector('#yell').addEventListener('click', tempC)
 //  get a starting temp
-
+function tempC() {
    // convert it to a different temp scale
-    temp = temp * 9/5 + 32
-   // return/output the converted temp
-    
+   const temp = document.querySelector('#cel').value
+   let tempConvert = temp * 9/5 + 32
+   // console.log('Hello World')
 
+     // return/output the converted temp
+   document.querySelector('#yellTemp').innerText = `${tempConvert}`
+
+}
+   
+ 
     
 
