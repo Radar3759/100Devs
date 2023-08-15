a056c0e735444571767a69a8340522615da5ec65
Radar3759 <chainmailgirl@gmail.com>
2023-08-15 13:52:15 -0700

    -add review summer games

--- class20-materials/review-summer-games/js/main.js
+++ class20-materials/review-summer-games/js/main.js
@@ -1,1 +1,15 @@
 //Create a function that takes in an array of numbers. Multiply each number together and alert the product. 
+arr = [3, 5, 10, 2, 1]
+
+function multiplyArr(arr) {
+    let total  = 1
+
+    for (let i = 0; i < arr.length; i++) {
+        total = total * arr[i]
+    }
+        alert(total)
+    }
+
+multiplyArr(arr)
+// alternate solution from Leon
+// arr.forEach( num => total += num)
