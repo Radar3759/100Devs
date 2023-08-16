3c3ccf6ff4c2a2df3062a58441040cbc71541512
Radar3759 <chainmailgirl@gmail.com>
2023-08-16 12:07:28 -0700

    -add alien rangers, obj zeo hmwk

--- class21-materials/alien-rangers-review/js/main.js
+++ class21-materials/alien-rangers-review/js/main.js
@@ -1,9 +1,26 @@
 //Arrays
 
 //Create and array of tv shows. Loop through and print each show to the console
+let tvShows = ["Manimal", "Maude", "The Simpsons", "Green Acres", "The Bachelor"]
+
+tvShows.forEach( i => console.log(i))
 
 //Create and array of numbers
 //Return a new array of numbers that includes every even number from the previous Arrays
+arrayOfNums = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// evenNums is a f(x) takes param arr, filter method on arr
+let evenNums = arr => arr.filter( n => n % 2 === 0 )
+console.log(evenNums(arrayOfNums))
 
 //Create a function that takes in an array of numbers
 //Alert the sum of the second lowest and the second highest number
+arrayForHighLow = [ 10, 20, 30, 40, 50, 60]
+
+function numHighLow(arr) {
+    let sorted = arr.sort((a,b) => a-b)
+    alert(sorted [1] + sorted[sorted.length -2])
+
+}
+
+numHighLow(arrayForHighLow)
--- class21-materials/objects-zeo/js/main.js
+++ class21-materials/objects-zeo/js/main.js
@@ -1,1 +1,19 @@
 //Create a stopwatch object that has four properties and three methods
+let stopwatch = {}
+
+stopwatch.hours = 24
+stopwatch.size = 'medium'
+stopwatch.color = "green"
+stopwatch.shape = "square"
+
+stopwatch.tellColor = function() {
+    console.log(`The color of the stopwatch is ${stopwatch.color}`)
+}
+
+stopwatch.stop = function () {
+    console.log('Stop')
+}
+
+stopwatch.alarm = function() {
+    alert("Alarmmmmm!!!")
+}
