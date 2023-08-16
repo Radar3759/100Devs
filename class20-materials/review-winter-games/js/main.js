df8928c84d83afb0cd09e4a5d07e95b7e70eb4b5
Radar3759 <chainmailgirl@gmail.com>
2023-08-16 10:08:32 -0700

    -add review winter games

--- class20-materials/review-winter-games/js/main.js
+++ class20-materials/review-winter-games/js/main.js
@@ -1,1 +1,32 @@
 //Create a function that takes in an array of numbers. Return a new array containing every even number from the original array (do not use map or filter)
+
+arr1 = [2, 5, 10, 15]
+
+function evenNumsLoop(arr) {
+    let newArr = []
+
+    for (let i = 0; i <= arr.length; i++) {
+        if ( i % 2 === 0) {
+            console.log(i)
+            newArr.push(i)
+        } 
+    }   
+    return newArr
+}
+
+
+console.log(evenNumsLoop(arr1))
+
+function evenNums(arr) {
+    let newArr = []
+
+    arr.forEach ( n => {
+        if( n % 2 === 0) {
+            // console.log(n)
+            newArr.push(n)
+        }
+    })
+    return newArr
+}
+   
+console.log(evenNums(arr1))
