f73e2faa582590606144d8206bda53df6975426d
Radar3759 <chainmailgirl@gmail.com>
2023-08-18 15:07:29 -0700

    -add espresso machine

--- class30-materials/objects-espresso-machine/js/main.js
+++ class30-materials/objects-espresso-machine/js/main.js
@@ -1,1 +1,21 @@
-//Create an espresso machine constructor that makes machines with 4 properties and 3 methods
+//Create an espresso machine constructor that makes machines with 4 properties and 3 methods]
+//No parens after EspressoMachine because it's a constructor, not a constructor f(x)
+class EspressoMachine {
+    constructor(color, make, model, price) {
+        this.color = color
+        this.make = make
+        this.model = model
+        this.price = price
+    }
+    turnOn(){
+        console.log("Hi, I'm on now.")
+    }
+    steam(){
+        console.log("Steeeeammmmmyyyy")
+    }
+    brew() {
+        console.log("Brewwwwinggg!")
+    }
+}
+
+let gaggia = new EspressoMachine('red', 'Gaggia', 'Classic Pro', 400)
